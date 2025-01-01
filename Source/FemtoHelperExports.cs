446ffacbde36481c09d7d57a3fbcd793348acca2
xestevaogspx@gmail.com <xestevaogspx@gmail.com>
2025-01-01 09:19:49 -0400

    wtf is this indentation

--- Source/FemtoHelperExports.cs
+++ Source/FemtoHelperExports.cs
@@ -6,49 +6,49 @@ namespace Celeste.Mod.FemtoHelper;
 
 public static class FemtoHelperExports
 {
-	[ModExportName("FemtoHelper.SMWBlockInfo")]
-	public static class SmwBlockInfo
-	{
-		public static void GetHitMethod(Entity block, Player player, int dir) => (block as Generic_SMWBlock)?.Hit(player, dir);
+    [ModExportName("FemtoHelper.SMWBlockInfo")]
+    public static class SmwBlockInfo
+    {
+        public static void GetHitMethod(Entity block, Player player, int dir) => (block as Generic_SMWBlock)?.Hit(player, dir);
 
-		public static bool IsActive(Entity block)
-		{
+        public static bool IsActive(Entity block)
+        {
             return (block as Generic_SMWBlock)?.Active ?? false;
-		}
-		public static bool CanHitTop(Entity block)
-		{
-			return (block as Generic_SMWBlock)?.CanHitTop ?? false;
-		}
-		public static bool CanHitBottom(Entity block)
-		{
-			return (block as Generic_SMWBlock)?.CanHitBottom ?? false;
-		}
-		public static bool CanHitLeft(Entity block)
-		{
-			return (block as Generic_SMWBlock)?.CanHitLeft ?? false;
-		}
-		public static bool CanHitRight(Entity block)
-		{
-			return (block as Generic_SMWBlock)?.CanHitRight ?? false;
-		}
-	}
+        }
+        public static bool CanHitTop(Entity block)
+        {
+            return (block as Generic_SMWBlock)?.CanHitTop ?? false;
+        }
+        public static bool CanHitBottom(Entity block)
+        {
+            return (block as Generic_SMWBlock)?.CanHitBottom ?? false;
+        }
+        public static bool CanHitLeft(Entity block)
+        {
+            return (block as Generic_SMWBlock)?.CanHitLeft ?? false;
+        }
+        public static bool CanHitRight(Entity block)
+        {
+            return (block as Generic_SMWBlock)?.CanHitRight ?? false;
+        }
+    }
 
     [ModExportName("FemtoHelper.PlutoniumTextInfo")]
     public static class PlutoniumTextInfo
-	{
-		public static object CreateTextEffectData(bool wavey, Vector2 amp, float offset, bool shakey, float amount, bool obfuscated, bool twitchy, float twitchChance, float phaseIncrement, float waveSpeed)
-		{
-			return new TextEffectData(wavey, amp, offset, shakey, amount, obfuscated, twitchy, twitchChance, phaseIncrement, waveSpeed);
-		}
-
-		public static object EmptyTextEffectData() => new TextEffectData();
-
-		public static Component CreatePlutoniumTextComponent(string fontPath, string charList, Vector2 fontSize) => new PlutoniumText(fontPath, charList, fontSize);
-
-		public static void PrintPlutoniumText(Component text, Vector2 pos, string str, bool shadow, int spacing, Color mainColor, Color outlineColor, object TextEffectData /* create an empty one for no effects */, float scale /* = 1 */, int id /* = 0 */)
+    {
+        public static object CreateTextEffectData(bool wavey, Vector2 amp, float offset, bool shakey, float amount, bool obfuscated, bool twitchy, float twitchChance, float phaseIncrement, float waveSpeed)
         {
-			(text as PlutoniumText)?.Print(pos, str, shadow, spacing, mainColor, outlineColor, (TextEffectData as TextEffectData) ?? new TextEffectData(), 1, 0);
-		}
+            return new TextEffectData(wavey, amp, offset, shakey, amount, obfuscated, twitchy, twitchChance, phaseIncrement, waveSpeed);
+        }
+
+        public static object EmptyTextEffectData() => new TextEffectData();
+
+        public static Component CreatePlutoniumTextComponent(string fontPath, string charList, Vector2 fontSize) => new PlutoniumText(fontPath, charList, fontSize);
+
+        public static void PrintPlutoniumText(Component text, Vector2 pos, string str, bool shadow, int spacing, Color mainColor, Color outlineColor, object TextEffectData /* create an empty one for no effects */, float scale /* = 1 */, int id /* = 0 */)
+        {
+            (text as PlutoniumText)?.Print(pos, str, shadow, spacing, mainColor, outlineColor, (TextEffectData as TextEffectData) ?? new TextEffectData(), 1, 0);
+        }
 
         public static void PrintPlutoniumTextCentered(Component text, Vector2 pos, string str, bool shadow, int spacing, Color mainColor, Color outlineColor, object TextEffectData /* create an empty one for no effects */, float scale /* = 1 */, int id /* = 0 */)
         {
@@ -56,11 +56,11 @@ public static class FemtoHelperExports
         }
     }
 
-	internal static void Initialize()
-	{
-		typeof(SmwBlockInfo).ModInterop();
-		typeof(PlutoniumTextInfo).ModInterop();
-	}
+    internal static void Initialize()
+    {
+        typeof(SmwBlockInfo).ModInterop();
+        typeof(PlutoniumTextInfo).ModInterop();
+    }
 }
 
 
